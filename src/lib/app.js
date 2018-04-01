77fe5210ead63a92b7de2dd4909ee2e328a50e5e
bill bittner <bittner.w@gmail.com>
2018-03-31 18:49:15 -0700

    removed console logs from app.js

--- src/lib/app.js
+++ src/lib/app.js
@@ -3,15 +3,11 @@ import { Route, Switch } from 'react-router-dom';
 import { dynamicImport } from './utils/dynamicImport';
 
 const customizedApp = ({customPages}) => {
-    console.log('customPages:', customPages);
-    // note: update this to only take in a custom page config and not the whole siteConfig,
-    // otherwise client might receive sensative data from siteConfig
     const HomePage = dynamicImport('HomePage', customPages) || require('./pages/HomePage').default;
     const AboutPage = dynamicImport('AboutPage', customPages) || require('./pages/AboutPage').default;
     const LoginPage = dynamicImport('LoginPage', customPages) || require('./pages/LoginPage').default;
     const ShowPage = dynamicImport('ShowPage', customPages) || require('./pages/ShowPage').default;
     const FourOhFourPage = dynamicImport('pages/FourOhFourPage', customPages) || require('./pages/FourOhFourPage').default;
-
     return () => {
         return (
             <Switch>
