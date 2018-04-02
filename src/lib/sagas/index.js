fc4de96d9942a9d931c115bf5f4e5f290faa7c50
bill bittner <bittner.w@gmail.com>
2018-04-02 11:24:38 -0700

    updated sagas export to include handleShowPageUri and rebuilt

--- src/lib/sagas/index.js
+++ src/lib/sagas/index.js
@@ -1,16 +1,12 @@
 import { rootSaga } from './rootSaga';
-import { watchHandleShowPageUri } from './show_uri';
-import { watchNewAssetRequest } from './show_asset';
-import { watchNewChannelRequest, watchUpdateChannelClaims } from './show_channel';
-import { watchFileIsRequested } from './file';
-import { watchPublishStart } from './publish';
+// import { watchHandleShowPageUri } from './show_uri';
+// import { watchNewAssetRequest } from './show_asset';
+// import { watchNewChannelRequest, watchUpdateChannelClaims } from './show_channel';
+// import { watchFileIsRequested } from './file';
+// import { watchPublishStart } from './publish';
+import { handleShowPageUri } from './show_uri';
 
 export default {
   rootSaga,
-  watchHandleShowPageUri,
-  watchNewAssetRequest,
-  watchNewChannelRequest,
-  watchUpdateChannelClaims,
-  watchFileIsRequested,
-  watchPublishStart
+  handleShowPageUri,
 }
